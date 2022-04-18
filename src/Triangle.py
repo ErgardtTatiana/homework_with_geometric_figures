b510fbf2bf854d684be9cae8f610d69b36c2efd9
Tatiana <ergardttatyana@yandex.ru>
2022-04-18 17:17:55 +0300

    requirements added

--- src/Triangle.py
+++ src/Triangle.py
@@ -11,8 +11,7 @@ class Triangle(Figure):
         self.side1 = side1
         self.side2 = side2
         self.side3 = side3
-
-        if (side1 <= 0) or (side2 <= 0) or (side3 <= 0):
+        if (side1 <= 0) or (side2 <= 0) or (side3 <=0):
             raise ValueError('Передана не геометрическая фигура')
         if ((side1) == (side2)) and ((side2) == (side3)):
             print('Треугольник равносторонний')
@@ -21,17 +20,23 @@ class Triangle(Figure):
         Triangle.get_square(self)
         Triangle.get_perimetr(self)
 
+
+
     def get_square(self):
         self.hp = (self.side1 + self.side2 + self.side3) / 2
         self.area = math.sqrt(self.hp * (self.hp - self.side1) * (self.hp - self.side2) * (self.hp - self.side3))
         print('is area', self.name, self.area)
+
 
     def get_perimetr(self):
         self.perimetr = self.side1 + self.side2 + self.side3
         print('is perimetr', self.name, self.perimetr)
 
 
+
 triangle1 = Triangle('triangle1', 9, 12, 15)
 triangle2 = Triangle('triangle2', 11, 20, 14)
 triangle3 = Triangle('triangle3', 11, 4, 11)
-triangle1.add_area(triangle2)
+print("summ of area", triangle1.add_area(triangle2))
+
+
